736e9c7f12c8578f869979b5851f9dab0f66b31d
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-04 02:32:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidaysTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
-}
-function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}!`
-}
